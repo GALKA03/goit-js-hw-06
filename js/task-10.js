b9cabf5329168274f883ddc56168c735fef832af
Galina Matvienko <galyagmgm@gmail.com>
2022-09-11 10:33:17 +0200

    рр
    ооо

--- js/task-10.js
+++ js/task-10.js
@@ -12,28 +12,43 @@ console.log(refs.divBoxes)
 
 
 
-
-
-
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-function onCreateBoxes(amount){
-console.log(amount += 1)
-const createDiv = document.createElement('div')
-
-if(createDiv = amount.value){
-  createDiv.append(refs.divBoxes)
+function onCreateBoxes(){
+let amount  = + refs.divBoxes.value; 
+createBoxes(amount)
 };
+function createBoxes(amount){
+const firstDiv = 30;
+const elements = [];
+ for (var i = 0; i < amount; i++) {
+  let size = firstDiv + i * 10;
+const createDiv = document.createElement('div')
+createDiv.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgba( ${getRandomHexColor()} )`;
+elements.push(createDiv)
 
+};
+return elements;
+let boxesToAdd = createBoxes(inputNum.value)
+	boxes.append(...boxesToAdd)
 }
-
+//const createDiv = document.createElement('div')
+// if(createDiv = amount.value){
+//   createDiv.append(refs.divBoxes)}
 refs.createButton.addEventListener('click', onCreateBoxes)
-//refs.destroyButton.addEventListener('click', onDestroyBoxes)
+refs.destroyButton.addEventListener('click', onDestroyBoxes)
+
+
+
+
 
 
 // Создай функцию createBoxes(amount), которая принимает 
 // один параметр - число. Функция создает столько <div>,
 //  сколько указано в amount и добавляет их в div#boxes.
+
+// Размеры самого первого <div> - 30px на 30px.
+// Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
+// Все элементы должены иметь случайный цвет фона в формате HEX. Используй готовую функцию getRandomHexColor для получения цвета.
